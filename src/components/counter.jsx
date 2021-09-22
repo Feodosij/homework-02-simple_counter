7fb780734d7cd3b6ac22ab928e92a1411d141a51
fedya <feodosiyshapovalov@gmail.com>
2021-09-22 17:36:50 +0300

    counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const tags = [];
+
+  const formatCount = () => {
+    return count === 0 ? "nol" : count;
+  };
+
+  const getBageclasses = () => {
+    let classes = "badge m-2 bg-";
+    classes += count === 0 ? "danger" : "primary";
+    return classes;
+  };
+
+  const handleIncrement = (productId) => {
+    console.log(productId);
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+  };
+
+  return (
+    <>
+      <span className={getBageclasses()}>{formatCount()}</span>
+      <button
+        onClick={() => handleIncrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Increment
+      </button>
+      <button onClick={handleDecrement} className="btn btn-secondary btn-sm">
+        Decrement
+      </button>
+    </>
+  );
+};
+
+export default Counter;
